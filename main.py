b55625b47aafdacc9f7d9c7184348d7e38621472
Omar Fares <omar.fsm02@gmail.com>
2023-04-25 16:56:56 -0400

    Splitting the dataset into the Training set and Test set and feature scaling

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ import tensorflow as tf
 from sklearn.preprocessing import LabelEncoder
 from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import OneHotEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
 
 
 print(tf.__version__)
@@ -20,6 +22,10 @@ X[:, 2] = le.fit_transform(X[:, 2])
 
 ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [1])], remainder='passthrough')
 X = np.array(ct.fit_transform(X))
-
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+#feature scaling
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
 
 print(X)
