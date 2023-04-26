8b9575849732ef4b18abf2e30df9f5b4795e16bc
Omar Fares <omar.fsm02@gmail.com>
2023-04-25 22:16:17 -0400

    more tests

--- main.py
+++ main.py
@@ -43,3 +43,9 @@ ann.fit(X_train, y_train, batch_size=32, epochs=100)
 
 print(X)
 print(ann.predict(sc.transform([[1, 0, 0, 600, 1, 40, 3, 60000, 2, 1, 1, 50000]])) > 0.5)
+print(ann.predict(X_train)) # Use X_train instead of y_train
+print(ann.predict(X_train) > 0.5) # Use ann.predict(X_train) instead of ann.preidct(y_train > 0.5)
+
+y_pred = ann.predict(X_test)
+y_pred = (y_pred > 0.5)
+print(np.concatenate((y_pred.reshape(len(y_pred),1), y_test.reshape(len(y_test),1)),1))
